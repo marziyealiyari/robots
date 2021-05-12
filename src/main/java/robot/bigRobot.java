c4e9d42c5ba315d439af395a14fb5a7c1ce296eb
mirziyealiyari <marziyealiyariuni@gmail.com>
2021-05-12 12:17:31 +0430

    message changing

--- src/main/java/robot/bigRobot.java
+++ src/main/java/robot/bigRobot.java
@@ -8,7 +8,6 @@ public class bigRobot extends robots {
     public void update(Observable o, Object arg) {
         super.update(o, arg);
         System.out.println("message bigRobot");
-     //   System.out.println("test");
     }
 
 }
